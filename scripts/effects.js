8c95876fddd0afb7b6aeb979091e5a26fa2420d1
hews <sam.hewamanage@tessella.com>
2015-03-24 16:12:34 -0400

    Produces effects for main.

---
+++ scripts/effects.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#ldiv').illuminate({
+    'intensity': '0.3',
+    'color': '#98cb00',
+    'blink': 'true',
+    'blinkSpeed': '1200',
+    'outerGlow': 'true',
+    'outerGlowSize': '30px',
+    'outerGlowColor': '#98cb00'
+    });
+});
