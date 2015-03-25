d854445b8735b71c35723be5cf532f7a868e89b0
hews <sam.hewamanage@tessella.com>
2015-03-25 17:12:43 -0400

    Adding more features and contents. closer to final product.

--- scripts/effects.js
+++ scripts/effects.js
@@ -11,21 +11,329 @@
 });
 */
 
+
+var pen_text = "<H3>Key Skills</H3> I joined Tessella after finishing my \
+                post-doc in high energy particle physics at Fermilab. While at \
+                Tessella I worked on number of different projects for both \
+                pharmaceutical and oil and gas industry and learned new skills \
+                in the process. I have experience working with both waterfall \
+                and agile methodologies.  I work well both independently and \
+                as part of wider teams and has an excellent background working \
+                closely with clients to understand their needs and requirements. \
+                I have worked on several international scientific research \
+                projects that used terabytes of particle collision data. \
+                I contributed to the data acquisition, data cleaning, \
+                data transformation, and data analysis work in these \
+                collaborations. Through these research projects I mastered \
+                object-oriented programming and gained experience in C++, \
+                Python, R, and HTML. Presenting my work at international \
+                conferences and collaboration meetings I have developed \
+                excellent communication skills. I enjoy learning new \
+                technologies and using them to solve complex problems.";
+
+var qua_text = "<div class='divsubitem'> Developed software to analyze historical social content tags and provide suggestions for new contents</div> \
+                <div class='divsubitem'> Developing an adaptive drug supply simulation study for phase &#189; clinical trials</div> \
+                <div class='divsubitem'> Designed and developed tools for Sales Team to perform <q>What if Analysis</q></div> \
+                <div class='divsubitem'> Contributed to the development of software packages that aids the design and planning of clinical trials based on Bayesian statistics.</div> \
+                <div class='divsubitem'> Developed software tool to port information across MS Office products.</div> \
+                <div class='divsubitem'> Developed data analyses frameworks and tools using OOP design for an international collaboration.</div> \
+                <div class='divsubitem'> Designed and developed anomalous data filters.</div> \
+                <div class='divsubitem'> Administer and developed software release validation tools and visualizations.</div>  \
+                <div class='divsubitem'> Provided online and offline support for data acquisition for an international collaboration. Developed GUI based software for the DAQ system.</div>  \
+                ";
+
+var pub_text = "<h2>Selected Publications</h2> \
+                <div class='divsubitem'> CMS Collaboration, &#8220; Search for new physics in the multijet and missing transverse momentum final state in proton-proton collisions at 8 TeV &#8221; , JHEP 06 (2014) 055</div> \
+                <div class='divsubitem'> CMS Collaboration, &#8220; Search for new physics in the multijet and missing transverse momentum final state in proton-proton collisions at 7 TeV &#8221; , Phys. Rev. Lett. 109 (2012)</div> \
+                <div class='divsubitem'> CDF Collaboration,  &#8220; The CDF II eXtremely Fast Tracker upgrade &#8221; , Nucl. Inst. Meth. A 572, 358-360 (2007)</div> \
+                ";
+
+var pres_text = "<h2>Selected Presentations</h2> \
+                    <div class='divsubitem'> Search for New Physics using Large Hadron Collider Data, CMS Results at the Phenomenology Symposium, 2013</div> \
+                    <div class='divsubitem'> Search for New Physics using photon + jets events using Collider Detector at Fermilab data, Florida International University (2011)/ Texas Tech University (2010)/ National University of Taiwan (2009)/ American Physical Society (2008)</div> \
+                ";
+
+var train_text = "<h2>Recent Training Courses</h2> \
+                    <div class='divsubitem'>Self-study JavaScript, jQuery 2014/ 2015</div> \
+                    <div class='divsubitem'>Self-study C#, 2014</div> \
+                    <div class='divsubitem'>Self-study WPF, 2014</div> \
+                    <div class='divsubitem'>Computing for Data Analysis (in R) offered by Johns Hopkins on Coursera.org, 2013</div> \
+                    <div class='divsubitem'>Statistics One (in R) offered by Princeton University on Coursera.org, 2013</div> \
+                ";
+
+var emp_text = "\
+<h2>Employment History</h2> \
+<table> \
+<tr> \
+    <th>Start</th> \
+    <th>End</th> \
+    <th>Employer</th> \
+</tr> \
+<tr> \
+    <td>11/2014</td> \
+    <td>Present</td> \
+    <td>Tessella Inc.</td> \
+</tr> \
+<tr> \
+    <td>05/2011</td> \
+    <td>10/2014</td> \
+    <td>Florida International University</td> \
+</tr> \
+<tr> \
+    <td>05/2005</td> \
+    <td>04/2011</td> \
+    <td>Baylor University</td> \
+</tr> \
+<tr> \
+    <td>09/2002</td> \
+    <td>04/2004</td> \
+    <td>University of Cincinnati</td> \
+</tr> \
+<tr> \
+    <td>2001</td> \
+    <td>2002</td> \
+    <td>University of Colombo, Sri Lanka</td> \
+</tr> \
+<tr> \
+    <td>2001</td> \
+    <td>2002</td> \
+    <td>Institute of Technological Studies, Sri Lanka</td> \
+</tr> \
+<tr> \
+    <td>1995</td> \
+    <td>1996</td> \
+    <td>The Orient Club, Sri Lanka</td> \
+</tr> \
+";
+
+proj_text_temp = "<div id='accordian' \
+                <h3>Section 1</h3> \
+                <div> \
+                  <p> \
+                  Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer \
+                  ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit \
+                  amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut \
+                  odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate. \
+                  </p> \
+                </div> \
+                <h3>Section 2</h3> \
+                <div> \
+                  <p> \
+                  Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet \
+                  purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor \
+                  velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In \
+                  suscipit faucibus urna. \
+                  </p> \
+                </div> \
+                <h3>Section 3</h3> \
+                <div> \
+                  <p> \
+                  Nam enim risus, molestie et, porta ac, aliquam ac, risus. Quisque lobortis. \
+                  Phasellus pellentesque purus in massa. Aenean in pede. Phasellus ac libero \
+                  lacinia ornare, quam ante aliquam nisi, eu iaculis leo purus venenatis dui. \
+                  </p> \
+                  <ul> \
+                    <li>List item one</li> \
+                    <li>List item two</li> \
+                    <li>List item three</li> \
+                  </ul> \
+                </div> \
+            </div> \
+        ";
+
+proj_text = "\
+<h2>Suggestions for Social Content Tagging</h2> \
+- February - March 2015 <br> \
+- Skills: Requirement Gathering, Natural Language Processing, Python <br> \
+- Using historical data generate suggestions for users to tag social contents. \
+Provide analysis of frequent tag words, possible duplicates and spell corrections for the administrators. <br> \
+<h2>&#39What if Analysis&#39 Tool for Sales</h2> \
+- January - February 2015 <br> \
+- Skills: Requirement Gathering, Mathematics, Forecasting, Issue Tracking (JIRA), VB , Visual Studio <br> \
+- Automated collection and import of sales records and perform &#39What If Analysis&#39 \
+for different scenarios by generating summary reports and charts by sectors, \
+divisions and for the company. <br> \
+<h2> Clinical Trial Data Accrual and Planning Tool</h2> \
+- November - December 2014 <br> \
+- Skills: Agile Development, Team work, Unit Testing, Development Testing, \
+Deployment Testing, Issue Tracking (JIRA), C#, Visual Studio <br> \
+- Worked as part of a Tessella team carrying out a project for a global \
+Pharmaceutical company.  Designed and developed unit testing and system \
+testing for the trial data accrual and decision planning simulation tools. \
+These tools visualize and simulate the impact of availability of data on \
+certain aspects of clinical trial&#39s design and proper sample size for a \
+learning phase trial. Scrum project management under Agile development \
+framework was used with 3 sprints to deliver final product in 1.5 months. <br> \
+<h2>MS Office Data Migration Tool</h2> \
+- November - December 2014<br> \
+- Skills: VB, Visual Studio, User Interface Design, Client Management<br> \
+- Worked on a project for a major oil & gas company.  Developed code base \
+and a user interface to extract information from PowerPoint presentation into \
+MS Office products. The design had to be simple, intuitive and able to handle \
+various user scenarios. Choice of technology was based on client requirement \
+and the finish product was delivered in one week.<br> \
+<h2>Data Analyses and Operations Support for CMS Collaboration at LHC</h2> \
+- June 2011 - October 2014<br> \
+- Skills: C++, Python, HTML, Mathematical Modeling, Simulation, Statistics, \
+CVS, GIT, Grid Computing (LHCG, HTCondor)<br> \
+- Developed several high performance code bases to isolate rare events and \
+measure their physics properties. OOP design techniques were used to develop \
+code and share with international collaborators. They were designed to scale, \
+rapidly evolve and optimized for faster processing.<br> \
+- Developed plugins to isolate spurious DAQ events from data streams.  \
+The isolation criteria needed to be robust and efficient. Plugins needed \
+be portable across different versions of internal software versions.<br> \
+- Developed and administered software validation tools for the collaboration. \
+Internal software framework was rapidly evolving to adjust to the detector \
+conditions and incorporating new features. But the physics contents must \
+remain consistent. For this, implemented a visualization using Java Script to \
+summarize consistency of fundamental physics measurements in new software builds. <br> \
+<h2>Data Analyses and Operations Support for CDF Collaboration at Fermilab</h2> \
+- June 2005 - May 2011 <br> \
+- Skills: C++, C, Java, HTML, Mathematical Modeling, Simulation, Statistics, \
+CVS, Grid Computing (HTCondor) <br> \
+- Developed a several high performance code base to isolate rare events and \
+measure their physics properties. OOP design techniques were used to develop \
+code and share with international collaborators. It was designed to scale, \
+rapidly evolve and optimized for faster processing. Developed new data \
+structures to speed up processing time. <br> \
+- Developed Java based user interface for the collaboration to load different \
+design patterns to logic boards. It can be run in command line mode which was \
+crucial for collaborators working remotely. The new program reduced the data \
+recording downtime by half. <br> \
+- Developed highly customizable tools in Bash/Csh to query computer grid. <br> \
+";
+
+var tech_text = "\
+<h2>Technical Skills</h2> \
+<table border-> \
+<tr> \
+<td>Development Languages and Environments</td><td>C, C++, Java, SQL, C#</td> \
+</tr> \
+<tr> \
+<td>Scripting languages</td><td>R, Python, JavaScript, Bash, Csh</td> \
+</tr> \
+<tr> \
+<td>Markup Languages and Tools</td><td>HTML, LaTeX</td> \
+</tr> \
+<tr> \
+<td>Development Tools / Libraries</td><td>Vim, Emacs, GNU Make, XCode, STL, Visual Studio, PyCharm, Eclipse, WPF</td> \
+</tr> \
+<tr> \
+<td>Operating Systems</td><td>Linux (Scientific Linux, Red Hat, Ubuntu), Windows XP/7/8, OS X</td> \
+</tr> \
+<tr> \
+<td>Design / Architecture Experience</td><td>Object Oriented Design, Stand Alone</td> \
+</tr> \
+<tr> \
+<td>Specialist development packages</td><td>ROOT</td> \
+</tr> \
+<tr> \
+<td>Version Control Systems</td><td>CVS, GIT, SVN, Stash</td> \
+</tr> \
+<tr> \
+<td>Other</td><td>FrontPage, Dream Weaver, MS Office Suite, iWork, TeXShop</td> \
+</tr> \
+<tr> \
+<td>General</td><td>Monte Carlo Simulation, Mathematical Modeling</td> \
+</tr> \
+<tr> \
+<td>Issue/Bug Tracking</td><td>JIRA<td> \
+</tr> \
+<tr> \
+<td>Test and Integration Experience</td><td>Test Planning, Unit Testing, System Testing, Deployment Testing, Acceptance Testing, TeamCity use</td> \
+</tr> \
+</table> \
+";
+
+function adjustHeights(elem) {
+    var fontstep = 2;
+    if ($(elem).height()>$(elem).parent().height() || $(elem).width()>$(elem).parent().width()) {
+      $(elem).css('font-size',(($(elem).css('font-size').substr(0,2)-fontstep)) + 'px').css('line-height',(($(elem).css('font-size').substr(0,2))) + 'px');
+      adjustHeights(elem);
+    }
+}
+
 function OnMouseIn(elem) {
     //code
+    console.log("OnMouseOIn triggered by " + elem.id)
     //elem.style.border = "2px solid blue";
     //elem.style.background-color = "#99CCCC" ;
-    elem.style.border-color = "#ffccff";
+    //elem.style.border-color = "#ffccff";
 };
 
 function OnMouseOut(elem) {
-    ele.style.border = "";
+    console.log("OnMouseOut triggered by " + elem.id)
+    elem.style.border = "";
 }
 
-function changeContent(srdId, tgtId, msg) {
-	console.log("In changeContent");
-	var el = getElementById(srcId);
-	if (el) {
-		el.innerHTML = "SOMETHING NEW";
+function SetText(ele, txt) {
+    //code
+    if (ele) {
+        ele.innerHTML = txt;
+        adjustHeights(ele);
+    }
+
+}
+
+function ChangeContent(srcEle, tgtEle, msg) {
+	if (srcEle) {
+            console.log("ChangeContent triggered by " + srcEle.id);
+            var te = document.getElementById(tgtEle);
+            if (te) {
+                if (srcEle.id == "pen") {
+                    //te.innerHTML = pen_text;
+                    //adjustHeights(te);
+                    srcEle.fon
+                    SetText(te, pen_text);
+                } else if (srcEle.id == "qua") {
+                    SetText(te, qua_text);                    
+                } else if (srcEle.id == "pub") {
+                    SetText(te, pub_text);                    
+                } else if (srcEle.id == "pres") {
+                    SetText(te, pres_text);                    
+                } else if (srcEle.id == "train") {
+                    SetText(te, train_text);                    
+                } else if (srcEle.id == "emp") {
+                    SetText(te, emp_text);                    
+                } else if (srcEle.id == "proj") {
+                    SetText(te, proj_text);
+                } else if (srcEle.id == "tech") {
+                    SetText(te, tech_text);                    
+                }
+            } else {
+                console.log("ChangeContent: " + tgtEle + " not found!")
+            }
 	}
 }
+
+/*$(document).ready(function() {
+    $('ldiv').hover(
+        function() {
+            $('cdiv').stop.fadeTo('slow', 1);
+        },
+        function() {
+            $('cdiv').stop().fadeTo('slow', 0.4);
+        }
+    );
+
+    $("#proj").accordian();
+
+    
+});
+*/
+
+/*$(document).ready( function(){
+    $('#cdiv').illuminate();
+    $("#accordian").accordian();
+
+});
+*/
+
+/*$("#ldiv").click(function() {
+    $('#ldiv').fadeOut("slow", function() {
+        // Animation Complete
+    });
+});
+*/
+
--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from bottle import route, run, debug, static_file
 @route("/")
 def home():
     return static_file('index.html', root='')
+    #return static_file('test/test.html', root='')
 
 # Static files such as images or CSS files are not served automatically
 # Added a route and a callback to control which files get served and where to find them
