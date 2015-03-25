cbd9f9066b5f36edfaab237bf4d446c9990da1f7
samantha <samantha@cern.ch>
2015-03-24 23:52:05 -0400

    work in progress

--- scripts/effects.js
+++ scripts/effects.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
     $('#ldiv').illuminate({
     'intensity': '0.3',
     'color': '#98cb00',
@@ -9,14 +9,23 @@ $(document).ready(function(){
     'outerGlowColor': '#98cb00'
     });
 });
-
+*/
 
 function OnMouseIn(elem) {
     //code
     //elem.style.border = "2px solid blue";
-    elem.style.background-color: "#99CCCC" ;
-}
+    //elem.style.background-color = "#99CCCC" ;
+    elem.style.border-color = "#ffccff";
+};
 
 function OnMouseOut(elem) {
     ele.style.border = "";
+}
+
+function changeContent(srdId, tgtId, msg) {
+	console.log("In changeContent");
+	var el = getElementById(srcId);
+	if (el) {
+		el.innerHTML = "SOMETHING NEW";
+	}
 }
